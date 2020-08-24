85ecfdea4ea711d3da789ee8d0d0cadb6ebb1195
MechanicJohn <solidfire444@gmail.com>
2020-08-23 22:51:34 -0600

    portscan 1.1

--- portscan.pl
+++ portscan.pl
@@ -7,17 +7,30 @@ use Socket;
 use Data::Validate::Domain;
 use IO::Socket;
 use Win32::Env;
+use Net::Ping;
 
 
 # TCP Port scanner
-my $VERSION = '1.0';
+my $VERSION = '1.1';
 $| = 1; # so \r works right, # flush the print buffer
+$SIG{INT} = \&interrupt;
+my $LOG_FILE;
 
-my ($ip, $protocol, $start_port, $end_port, $log, $domain, $address);
+my ($ip, $protocol, $start_port, $end_port, $log, $domain, $address, $path);
 $protocol = getprotobyname('tcp');
 ($ip, $start_port, $end_port, $log) = @ARGV;
 
-print "PORTSCAN v$VERSION\n";
+print " PORTSCAN v$VERSION\n";
+
+
+my $p = Net::Ping->new;
+if ($p->ping($ip, 1)) {
+    print "Host is reachable\n";
+}
+else{
+	 print "Host is not reachable\n";
+}
+
 
 
 if ($ip eq "-h") {
@@ -25,7 +38,11 @@ if ($ip eq "-h") {
 	exit 0;
 }
 
-my $path = $ENV{'USERPROFILE'};
+if (!$ip) {
+	usage()
+}
+
+$path = $ENV{'USERPROFILE'};
 $path =~s/\\/\//g;
 $domain = $ip;
 $ip = "localhost" if not $ip;
@@ -34,6 +51,7 @@ $end_port = 1024 if not $end_port;
 $log = "$path\portscan.txt" if not $log;
 my $valid = is_domain($domain);
 
+
 if ( $valid )
 {
 	my $resolve = inet_aton($domain);
@@ -41,7 +59,7 @@ if ( $valid )
 	if ($resolve) {
 		$address = inet_ntoa($resolve);
 	}
-	print "$domain $address \n";
+	print " $domain $address \n";
 	$ip = $address;
 }
 
@@ -52,33 +70,44 @@ if($ip !~  m/^\d+\.\d+\.\d+\.\d+$/)
 	exit 0;
 }
 
-if ( $start_port > $end_port )
+
+if($ip !~  m/^\d+\.\d+\.\d+\.\d+$/ and $ip ne "localhost")
 {
 	usage();
-	print "start_port can't be greater then the end_port.\n";
+	print " Invalid Host $ip\n";
 	exit 0;
 }
 
 
-unless (open(LOG_FILE, ">>$log")) {
-    die "Can't open log file $log for writing: $!\n"
+if ( $start_port > $end_port )
+{
+	usage();
+	print " start_port can't be greater then the end_port. We cant backwards yo.\n";
+	exit 0;
+}
+
+
+unless (open($LOG_FILE, ">>$log")) {
+    die " Can't open log file $log for writing: $!\n"
 }
 
 # Make file handle hot so the buffer is flushed after every write
-select((select(LOG_FILE), $| = 1)[0]);
+select((select($LOG_FILE), $| = 1)[0]);
 
-print "Checking $ip for open ports $start_port - $end_port\n";
+print " Scanning $ip for open ports $start_port - $end_port\n";
 
 my $ports;
 my @open_ports = ();
 
+print " Press CTRL + C to terminate scan\n";
+
 foreach (my $port = $start_port ; $port <= $end_port ; $port++) 
 {
     #\r will refresh the line
-    print "\rScanning port $port";
+    print "\r Scanning UDP port $port";
      
     #Connect
-    my $socket = IO::Socket::INET->new(PeerAddr => $ip , PeerPort => $port , Proto => 'tcp' , Timeout => 1);
+    my $socket = IO::Socket::INET->new(PeerAddr => $ip , PeerPort => $port , Proto => 'udp' , Timeout => 1);
 	 
     #Check connection
     if( $socket )
@@ -89,31 +118,43 @@ foreach (my $port = $start_port ; $port <= $end_port ; $port++)
 		$ports++;
 		#push port to array
 		push (@open_ports, $port);
-        print "\nPort $port is open.\n" ;
+        print "\n Port $port is open.\n" ;
 		#return if ($port = $end_port);
     }
 }
+
+
+
 my $datestring = gmtime();
-print LOG_FILE "GMT date and time $datestring\n";
+print $LOG_FILE "GMT date and time $datestring\n";
 #my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
 #print LOG_FILE "$mon/$mday/$year";
 if ($ports) {
 	print "\n";
-	print "The following ports are open on $ip ";
-	print LOG_FILE "The following ports are open on $ip ";
+	print " The following ports are open on $ip ";
+	print $LOG_FILE "The following ports are open on $ip ";
 	
 	foreach my $data(@open_ports) { 
 		print "$data ";
-		print LOG_FILE "$data ";
+		print $LOG_FILE "$data ";
 	}
 }
 
 print "\n";
-print LOG_FILE "\n";
-print LOG_FILE "The following ports where tested on $ip between port $start_port and $end_port\n\n";
-close LOG_FILE || die "close: $!";
+print $LOG_FILE "\n";
+print $LOG_FILE "Finished Scanning $ip between port $start_port and $end_port\n\n";
+close $LOG_FILE || die "close: $!";
+print "\n";
+usage();
+exit 0;
 
 sub usage() {
-    print "Usage: portscan [host] [start_port] [end_port] [logfile]\n";
-    print "Defaults to localhost and port 1 and port 1024 portscan.txt\n";
+    print " Usage: portscan [host] [start_port] [end_port] [logfile]\n";
+    print " Defaults: portscan localhost 1 1024 portscan.txt\n";
+}
+
+sub interrupt {
+	close $LOG_FILE;
+	print "\n Terminating Port Scan\n";
+    exit;
 }
